cd2ea74335c162555a366f6ca9a053f9dff01d76
Billy Lynch <wlynch@google.com>
2022-01-26 16:47:46 -0500

    use combined image.

--- cosign.sh
+++ cosign.sh
@@ -6,5 +6,4 @@ IMAGE=$(ko publish .)
 
 gcloud auth list 
 
-go install github.com/sigstore/cosign/cmd/cosign@21e6b806143652331b77c29c62a131fb33a71ae0
-COSIGN_EXPERIMENTAL=1 $(go env GOPATH)/bin/cosign sign --force --identity-token=$(gcloud auth print-identity-token --audiences=sigstore) ${IMAGE}
+COSIGN_EXPERIMENTAL=1 $(go env GOPATH)/bin/cosign sign --force --k8s-keychain ${IMAGE}
