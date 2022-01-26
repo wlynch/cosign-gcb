3a04175d5fec98b12427fcd40cd525181c260bd8
Billy Lynch <wlynch@google.com>
2022-01-26 16:56:41 -0500

    script cleanup

--- cosign.sh
+++ cosign.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-go env
 export KO_DOCKER_REPO="gcr.io/wlynch-test"
 IMAGE=$(ko publish .)
 
 gcloud auth list 
 
-COSIGN_EXPERIMENTAL=1 $(go env GOPATH)/bin/cosign sign --force --k8s-keychain ${IMAGE}
+COSIGN_EXPERIMENTAL=1 /root/go/bin/cosign sign --force --k8s-keychain ${IMAGE}
